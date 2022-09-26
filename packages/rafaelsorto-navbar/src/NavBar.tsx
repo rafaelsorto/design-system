8b9a9dcc9908032307aaea70ba260ab8cf5b0591
Rafael Sorto <rafael@analogrepublic.com>
2022-09-26 14:42:17 -0600

    first commit

---
+++ packages/rafaelsorto-navbar/src/NavBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface NavBarProps {
+  children: React.ReactNode;
+}
+
+export function NavBar() {
+  return <div>NavBar</div>;
+}
---
+++ packages/rafaelsorto-navbar/src/index.tsx
@@ -0,0 +1,2 @@
+import * as React from "react";
+export { NavBar, type NavBarProps } from "./NavBar";
